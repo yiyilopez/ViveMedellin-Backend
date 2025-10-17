9d15071eecc26d3eba03ed7e2d1a76e30accdda1
Yiyi <yiyi.lopez@udea.edu.co>
2025-10-17 18:49:10 -0500

    comments inncesarios

--- vivemedellinbackend/src/main/java/com/vivemedellin/controllers/CategoryController.java
+++ vivemedellinbackend/src/main/java/com/vivemedellin/controllers/CategoryController.java
@@ -18,30 +18,29 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    //create
     @PostMapping("/")
     public ResponseEntity<CategoryDto> createCategory(@Valid @RequestBody CategoryDto categoryDto){
         CategoryDto createdCategory = this.categoryService.createCategory(categoryDto);
         return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
     }
-    // update
+
     @PutMapping("/{categoryId}")
     public ResponseEntity<CategoryDto> updateCategory(@Valid @RequestBody CategoryDto categoryDto, @PathVariable Integer categoryId){
         CategoryDto updatedUser = this.categoryService.updateCategory(categoryDto,categoryId);
         return ResponseEntity.ok(updatedUser);
     }
-    //delete
+
     @DeleteMapping("/{categoryId}")
     public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Integer categoryId){
         this.categoryService.deleteCategory(categoryId);
         return new ResponseEntity(new ApiResponse("Category Deleted Successfully! ", true),HttpStatus.OK);
     }
-    //get
+
     @GetMapping("/{categoryId}")
     public ResponseEntity<CategoryDto> getCategory(@PathVariable Integer categoryId){
         return ResponseEntity.ok(this.categoryService.getCategory(categoryId));
     }
-    //getAll Categories
+
     @GetMapping("/")
     public ResponseEntity<List<CategoryDto>> getAllCategories(){
         return ResponseEntity.ok(this.categoryService.getAllCategories());
