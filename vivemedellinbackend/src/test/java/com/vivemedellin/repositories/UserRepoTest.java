c035189e5116478d29e0d3fcbaff34cc09f8217b
Carlos Castaño <carlose.castano@udea.edu.co>
2025-10-31 11:53:39 -0500

    UserRepoTest

---
+++ vivemedellinbackend/src/test/java/com/vivemedellin/repositories/UserRepoTest.java
@@ -0,0 +1,38 @@
+package com.vivemedellin.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import com.vivemedellin.models.User;;
+
+@DataJpaTest
+public class UserRepoTest {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Test
+    void shouldReturnUser_whenEmailExists() {
+        // Arrange
+        User testUser = new User(
+            "Test",
+            "user@gmail.com",
+            "1234",
+            "Soy un usuario de prueba"
+        );
+        User saved = userRepo.save(testUser);
+        // Act
+        Optional<User> foundOptional = userRepo.findByEmail(saved.getEmail());
+        // Assert
+        assertTrue(foundOptional.isPresent(), "El usuario debería existir en la base de datos");
+        User foundUser = foundOptional.get();
+        assertEquals("user@gmail.com", foundUser.getEmail(),
+            "El email del usuario encontrado debería coincidir con el de prueba");
+
+}
+
+}
