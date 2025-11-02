c41ff3825ec95fcba1b0b198dba21717654d5f3d
Yiyi <yiyi.lopez@udea.edu.co>
2025-11-02 17:19:33 -0500

    changes in user

---
+++ vivemedellinbackend/src/main/java/com/vivemedellin/payloads/OtpVerificationRequest.java
@@ -0,0 +1,15 @@
+package com.vivemedellin.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OtpVerificationRequest {
+    private String email;
+    private String otp;
+}
